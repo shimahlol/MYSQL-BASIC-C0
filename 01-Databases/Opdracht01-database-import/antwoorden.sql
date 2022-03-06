475c50d4ccbd63130bbc55dc567334e9a35dddae
jamirolol <2048415@talnet.nl>
2022-03-06 23:43:54 +0100

    done~

--- 01-Databases/Opdracht01-database-import/antwoorden.sql
+++ 01-Databases/Opdracht01-database-import/antwoorden.sql
@@ -1,1 +1,101 @@
 -- Taak01 - haal de tabel movies op:
+1
+Velvet Buzzsaw
+2019
+6
+2
+Alita: Battle Angel
+2019
+8
+3
+Avengers: Endgame
+2019
+0
+4
+Polar
+2019
+6
+5
+Bohemian Rhapsody
+2018
+8
+6
+Overlord
+2018
+7
+7
+Glass
+2019
+7
+8
+Green Book
+2018
+8
+9
+Aquaman
+2018
+7
+10
+Extremely Wicked, Shockingly Evil, and Vile
+2019
+8
+11
+Child's Play
+2019
+0
+12
+The Favourite
+2018
+8
+13
+Captain Marvel
+2019
+0
+14
+A Star Is Born
+2018
+8
+15
+Incredibles 2
+2018
+8
+16
+Fast & Furious Presents: Hobbs & Shaw
+2019
+0
+17
+The Lego Movie 2: The Second Part
+2019
+7
+18
+Joker
+2019
+0
+19
+Mortal Engines
+2018
+6
+20
+Cold Pursuit
+2019
+7
+21
+How to Train Your Dragon: The Hidden World
+2019
+8
+22
+Once Upon a Time in Hollywood
+2019
+0
+23
+Widows
+2018
+7
+24
+Robin Hood
+2018
+5
+25
+Roma
+2018
+8
