0bc9dcd9d0d3e582a2c5dba75956a0d7ffcd2f79
jamirolol <2048415@talnet.nl>
2022-03-10 12:31:26 +0100

    meer SQL

--- 02-Select/Opdracht04-AND-OR/antwoorden.sql
+++ 02-Select/Opdracht04-AND-OR/antwoorden.sql
@@ -1,30 +1,588 @@
 -- Opdracht 1 
-
+SELECT * FROM players WHERE nationality="spain" AND club="chelsea"
 -- Opdracht 2 
-
+SELECT * FROM players WHERE age=17 AND wage=15000 AND nationality="spain"
 -- Opdracht 3
-
+SELECT * FROM players WHERE club="Liverpool" AND age>=20
 -- Opdracht 4
-
+SELECT * FROM players WHERE club="Ajax" AND nationality="Netherlands"
 -- Opdracht 5
-
+SELECT * FROM players WHERE club="Ajax" AND nationality!="Netherlands"
 -- Opdracht 6 
-
+G. Coutinho 
+34
+R. Vlaar 
+32
+R. van Eijden 
+29
+S. Wuytens 
+27
+M. Vejinovi? 
+27
+J. Svensson 
+24
+F. Midtsj  
+23
+M. Bizot 
+26
+R. van Rhijn 
+26
+M. Seuntjens 
+25
+J. van Overeem 
+23
+A. Jahanbakhsh 
+23
+F. Friday 
+22
+I. Bel Hassani 
+24
+W. Weghorst 
+24
+N. Olij 
+21
+D. Dos Santos 
+20
+P. Hatzidiakos 
+20
+J. Helmer 
+19
+L. Garc a 
+19
+T. Ouwejan 
+21
+L. Opdam 
+21
+G. Til 
+19
+O. Wijndal 
+17
+C. Stengs 
+18
 -- Opdracht 7 
+G. Coutinho 
+34
+AZ Alkmaar 
+R. Vlaar 
+32
+AZ Alkmaar 
+R. van Eijden 
+29
+AZ Alkmaar 
+S. Wuytens 
+27
+AZ Alkmaar 
+M. Vejinovi? 
+27
+AZ Alkmaar 
+J. Svensson 
+24
+AZ Alkmaar 
+F. Midtsj  
+23
+AZ Alkmaar 
+M. Bizot 
+26
+AZ Alkmaar 
+R. van Rhijn 
+26
+AZ Alkmaar 
+M. Seuntjens 
+25
+AZ Alkmaar 
+J. van Overeem 
+23
+AZ Alkmaar 
+A. Jahanbakhsh 
+23
+AZ Alkmaar 
+F. Friday 
+22
+AZ Alkmaar 
+I. Bel Hassani 
+24
+AZ Alkmaar 
+W. Weghorst 
+24
+AZ Alkmaar 
+N. Olij 
+21
+AZ Alkmaar 
+D. Dos Santos 
+20
+AZ Alkmaar 
+P. Hatzidiakos 
+20
+AZ Alkmaar 
+J. Helmer 
+19
+AZ Alkmaar 
+L. Garc a 
+19
+AZ Alkmaar 
+T. Ouwejan 
+21
+AZ Alkmaar 
+L. Opdam 
+21
+AZ Alkmaar 
+G. Til 
+19
+AZ Alkmaar 
+O. Wijndal 
+17
+AZ Alkmaar 
+C. Stengs 
+18
+AZ Alkmaar 
 
 -- Opdracht 8
+Fernandinho 
+130000
+Danilo 
+95000
+Danilo 
+95000
+Ederson 
+87000
+Gabriel Jesus 
+115000
 
 -- Opdracht 9
+A. Turan 
+Piqu  
+S. Dann 
+L. Messi 
+W. Janssen 
+Cesc F bregas 
+H. Lloris 
+J. Peters 
+W. Hennessey 
+E. Garay 
+J. Vertonghen 
+S. Romero 
+L. Su rez 
+Kiko Casilla 
+David Luiz 
+M. Diam  
+K. Navas 
 
 -- Opdracht 10
-
+Cristiano Ronaldo 
+32
+David Villa 
+35
+Piqu  
+30
+Sergio Ramos 
+31
+Jes s G mez 
+32
+Cesc F bregas 
+30
+Eduardo 
+34
+David Silva 
+31
+Joselu 
+27
+Kiko Casilla 
+30
+Juan Mata 
+29
+Diego Costa 
+28
+Azpilicueta 
+27
+Jordi Alba 
+28
+Pedro 
+29
+Sergio Busquets 
+28
+Parejo 
+28
+Ander Herrera 
+27
+Marcos Alonso 
+26
+De Gea 
+26
+Osmar 
+29
+Montoya 
+26
+Isco 
+25
+Jes s Alfaro 
+26
 -- Opdracht 11
-
+A. Pirlo 
+New York City Football Club 
+K. Begois 
+FC Groningen 
+B. Ogbeche 
+Willem II 
+J. Saunders 
+Orlando City Soccer Club 
+G. Coutinho 
+AZ Alkmaar 
+Kak  
+Orlando City Soccer Club 
+Kim Chi Woo 
+FC Seoul 
+E. Braafheid 
+FC Utrecht 
+A. Nocerino 
+Orlando City Soccer Club 
+J. Spector 
+Orlando City Soccer Club 
+W. Johnson 
+Orlando City Soccer Club 
+Kwak Tae Hwi 
+FC Seoul 
+Park Chu Young 
+FC Seoul 
+R. Leeuwin 
+FC Utrecht 
+G. Barnes 
+Orlando City Soccer Club 
+Ha Dae Sung 
+FC Seoul 
+Jung In Whan 
+FC Seoul 
+Lee Sang Ho 
+FC Seoul 
+S. Sutter 
+Orlando City Soccer Club 
+R. Jenssen 
+FC Groningen 
+H. Jele 
+Orlando Pirates 
+D. Damjanovi? 
+FC Seoul 
+W. Sandilands 
+Orlando Pirates 
+S. Hines 
+Orlando City Soccer Club 
+M. Moralez 
+New York City Football Club
 -- Opdracht 12
-
+-- leeg is het antwoord
 -- Opdracht 13
+193440
+N. Viergever 
+27
+Netherlands 
+Ajax 
+65000000
+14000
+193706
+D. Dumi? 
+25
+Bosnia Herzegovina 
+FC Utrecht 
+33000000
+8000
+197981
+S. Makienok 
+26
+Denmark 
+FC Utrecht 
+11000000
+8000
+198147
+M. van der Maarel 
+27
+Netherlands 
+FC Utrecht 
+1000000
+7000
+198640
+Z. Labyad 
+24
+Morocco 
+FC Utrecht 
+85000000
+11000
+200698
+N. Marsman 
+26
+Netherlands 
+FC Utrecht 
+875000
+6000
+202547
+J. Bahebeck 
+24
+France 
+FC Utrecht 
+4000000
+42000
+203446
+A. Kali 
+26
+Morocco 
+FC Utrecht 
+37000000
+9000
+204102
+Y. Ayoub 
+23
+Morocco 
+FC Utrecht 
+10000000
+13000
+204367
+B. van Leer 
+25
+Netherlands 
+Ajax 
+7000000
+11000
+204370
+S. van de Streek 
+24
+Netherlands 
+FC Utrecht 
+23000000
+8000
+208004
+J. Veltman 
+25
+Netherlands 
+Ajax 
+85000000
+13000
+208459
+M. Dijks 
+24
+Netherlands 
+Ajax 
+55000000
+10000
+209696
+C. David 
+24
+Netherlands 
+FC Utrecht 
+500000
+2000
+213696
+M. Willock 
+20
+England 
+FC Utrecht 
+475000
+14000
+216380
+D. Sinkgraven 
+21
+Netherlands 
+Ajax 
+75000000
+9000
+219814
+S. Klaiber 
+22
+Netherlands 
+FC Utrecht 
+26000000
+7000
+221283
+A. Nouri 
+20
+Netherlands 
+Ajax 
+75000000
+10000
 
 -- Opdracht 14
-
+138412
+J. Milner 
+31
+England 
+Liverpool 
+95000000
+110000
+164468
+G. Cahill 
+31
+England 
+Chelsea 
+21000000
+160000
+171833
+D. Sturridge 
+27
+England 
+Liverpool 
+235000000
+140000
+180819
+A. Lallana 
+29
+England 
+Liverpool 
+25000000
+135000
+183427
+F. Delph 
+27
+England 
+Manchester City 
+10000000
+110000
+183711
+J. Henderson 
+27
+England 
+Liverpool 
+215000000
+115000
+186132
+D. Drinkwater 
+27
+England 
+Chelsea 
+14000000
+125000
+188377
+K. Walker 
+27
+England 
+Manchester City 
+24000000
+130000
+189881
+C. Smalling 
+27
+England 
+Manchester United 
+17000000
+120000
+190456
+N. Clyne 
+26
+England 
+Liverpool 
+22000000
+115000
+194957
+P. Jones 
+25
+England 
+Manchester United 
+125000000
+115000
+198784
+A. Oxlade-Chamberlain 
+23
+England 
+Liverpool 
+20000000
+105000
+202126
+H. Kane 
+23
+England 
+Tottenham Hotspur 
+59000000
+165000
+202652
+R. Sterling 
+22
+England 
+Manchester City 
+295000000
+140000
+203574
+J. Stones 
+23
+England 
+Manchester City 
+145000000
+105000
+211117
+D. Alli 
+21
+England 
+Tottenham Hotspur 
+43000000
+115000
 -- Opdracht 15
-
+J. Speroni 
+38
+Argentina 
+Fernandinho 
+32
+Brazil 
+Kak  
+35
+Brazil 
+W. Caballero 
+35
+Argentina 
+J. Mascherano 
+33
+Argentina 
+S. Ag ero 
+29
+Argentina 
+L. Messi 
+30
+Argentina 
+E. Garay 
+30
+Argentina 
+S. Romero 
+30
+Argentina 
+Marcelo 
+29
+Brazil 
+David Luiz 
+30
+Brazil 
+Willian 
+28
+Brazil 
+M. Moralez 
+30
+Argentina 
+Paulinho 
+28
+Brazil 
+Coutinho 
+25
+Brazil 
+N. Otamendi 
+29
+Argentina 
+Neto 
+27
+Brazil 
+Danilo 
+25
+Brazil 
+Danilo 
+25
+Brazil 
+Casemiro 
+25
+Brazil 
+Gabriel Paulista 
+26
+Brazil 
+Rafinha 
+24
+Brazil 
+M. Rojo 
+27
+Argentina 
+Roberto Firmino 
+25
+Brazil 
+Wallace Oliveira 
+23
+Brazil 
