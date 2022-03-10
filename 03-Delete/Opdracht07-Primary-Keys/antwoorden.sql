6092aa4c36f549efb822ce45d18c05e8e5b14ff1
jamirolol <2048415@talnet.nl>
2022-03-10 16:20:44 +0100

    PK

--- 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
+++ 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
@@ -1,5 +1,7 @@
 -- Opdracht 1
-
+-- gecheckt er zijn dubbele waardes in
 -- Opdracht 2
-
+DELETE FROM players where id=199304
 -- Opdracht 3
+DELETE FROM players where id=207494
+DELETE FROM players where id=226753
