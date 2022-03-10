24fd3e53e4607369eb6314b562b075a98686b979
jamirolol <2048415@talnet.nl>
2022-03-10 15:19:37 +0100

    delete

--- 03-Delete/Opdracht06-DELETE/antwoorden.sql
+++ 03-Delete/Opdracht06-DELETE/antwoorden.sql
@@ -1,9 +1,10 @@
 -- Opdracht 1 
-
+DELETE FROM players where name="David Silva"
 -- Opdracht 2 
-
+DELETE FROM players where club="Willem II"
 -- Opdracht 3
-
+DELETE FROM players where nationality="belgium" AND club="FC Barcelona"
 -- Opdracht 4
-
+DELETE FROM players where age >=38
 -- Opdracht 5
+DELETE FROM players where id=167905 OR id=169595
