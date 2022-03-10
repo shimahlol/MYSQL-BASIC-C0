b5a2d6dff66468766cc50bd8afe7e1fccf68d116
jamirolol <2048415@talnet.nl>
2022-03-10 15:09:10 +0100

    alias

--- 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
+++ 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
@@ -1,19 +1,33 @@
 -- Opdracht 1 
-
+hoogste_loon_speler_fc_utrecht
+42000
 -- Opdracht 2 
-
+afgeronde_gemiddelde_inkomen_van_de_spelers
+50240
 -- Opdracht 3
-
+loon_groningen_spelers
+135000
 -- Opdracht 4
-
+wage
+67
 -- Opdracht 5
-
+gemiddelde_loon_nederlandse_spelers
+12600.0000
 -- Opdracht 6 
-
+gemiddelde_loon_onder_20
+13321.6783
 -- Opdracht 7 
-
+57903.2258
 -- Opdracht 8
-
+gemiddelde_loon_boven_20
+57903.2258
 -- Opdracht 9
-
+gemiddelde_leeftijd
+24
 -- Opdracht 10
+club_naam
+totale_inkomen_of_uitgaven
+gemiddelde_waarde
+Liverpool 
+2664000
+60592187.5000
